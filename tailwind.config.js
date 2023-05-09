237f92056dc5307d9e64abfb7c285a2438d09d3a
Ernest Ho <ernest.heh@gmail.com>
2023-05-09 12:27:54 +0800

    change default sans and mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-env node */
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "class",
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["General Sans", "sans-serif"],
+      mono: ["Fragment Mono", "monospace"],
+    },
   },
   plugins: [],
 };
