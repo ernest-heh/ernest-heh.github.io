31f83e2c944876c0c8c3c31564e5b4bc6421e611
Ernest Ho <ernest.heh@gmail.com>
2023-05-09 12:49:38 +0800

    add Footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,12 @@
+import { FaCoffee } from "react-icons/fa";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center mt-auto mb-6 text-center text-xs tracking-[0.035rem]">
+      <footer>
+        Built with Vite, React, TailwindCSS. Fueled by Coffee{" "}
+        <FaCoffee className="inline text-[1rem]" />
+      </footer>
+    </div>
+  );
 }
