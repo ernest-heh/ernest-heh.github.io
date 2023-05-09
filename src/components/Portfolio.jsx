384a6478c5865eacb151ac7e04ceab5d1c001801
Ernest Ho <ernest.heh@gmail.com>
2023-05-09 12:44:42 +0800

    add PortfolioItem component to render in Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,3 +1,26 @@
+import portfolio from "../data/portfolio";
+import PortfolioItem from "./PortfolioItem";
+
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <>
+      <h2 className="text-2xl font-medium mt-20 mb-4">
+        Projects I&apos;ve Worked On
+      </h2>
+      <div className="flex flex-col md:flex-row items-center justify-center mb-10 pb-16 border-b border-stone-300 dark:border-stone-600">
+        <div className="grid grid-cols-1 gap-8">
+          {portfolio.map((project) => (
+            <PortfolioItem
+              key={project.title}
+              imgUrl={project.imgUrl}
+              title={project.title}
+              stack={project.stack}
+              link={project.link}
+              text={project.text}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/PortfolioItem.jsx
@@ -0,0 +1,26 @@
+export default function PortfolioItem({ title, imgUrl, stack, link, text }) {
+  return (
+    <div className="border border-stone-300 dark:border-stone-600 rounded-md overflow-hidden">
+      <img src={imgUrl} alt="portfolio" className="w-full h-120 object-cover" />
+      <div className="w-full p-4">
+        <h3 className="text-2xl md:text-3xl mb-2 md:mb-3 font-semibold">
+          {title}
+        </h3>
+        <p className="mb-4 tracking-[0.025rem]">{text}</p>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs mb-4">
+          {stack.map((item) => (
+            <span
+              key={item}
+              className="font-mono inline-block px-2 py-1 rounded-md border border-neutral-300 dark:border-neutral-400 text-neutral-500 dark:text-neutral-400"
+            >
+              {item}
+            </span>
+          ))}
+        </p>
+        <div className="flex gap-4 text-sm font-semibold">
+          <a href={link}>&rarr; View on GitHub</a>
+        </div>
+      </div>
+    </div>
+  );
+}
